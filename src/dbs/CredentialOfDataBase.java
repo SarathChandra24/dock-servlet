182ec833dd8df2286be6296906f76ad65baef6c9
sarathchandra24 <sarathchandra24@icloud.com>
2020-03-14 13:18:06 +0530

    from github app

--- src/dbs/CredentialOfDataBase.java
+++ src/dbs/CredentialOfDataBase.java
@@ -1,7 +1,7 @@
 package dbs;
 
 public class CredentialOfDataBase {
-    public static final String  DB_URL= "jdbc:mysql://172.18.0.2:3306/",
+    public static final String  DB_URL= "jdbc:mysql://10.128.2.22:3306/",
             // DB_URL= "jdbc:mysql://own-compose-mysql:3306/",
                                 DB_USER="root",
                                 DB_password="sarasu10",
